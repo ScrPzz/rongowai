b628349d9402c70edade3602533a36932d470785
Alessandro Togni <alessandro.togni@skf.com>
2025-06-06 17:15:02 +0200

    Preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,4 @@
-def preprocess_w_lat_lons(netcdf_file_path):
+def preprocess_w_lat_lons_time(netcdf_file_path):
     raw_counts = np.array(f.variables['raw_counts'])
             
     # Calcolo distanza tra il punto speculare e l'aereo
@@ -13,16 +13,22 @@ def preprocess_w_lat_lons(netcdf_file_path):
     dist = distance_2d[:]
     specular_point_lat = f.variables['sp_lat'][:]
     specular_point_lon = f.variables['sp_lon'][:]
-    
+    timestamps = f.variables['pvt_timestamp_utc'][:]
 
     keep_mask = (copol >= 5) & (xpol >= 5) & (snr > 0) & ((dist >= 2000) & (dist <= 10000)) & (~np.isnan(copol.data) & ~np.isnan(xpol.data) & ~np.isnan(snr.data) & ~np.isnan(dist.data) & ~np.isnan(specular_point_lat.data) & ~np.isnan(specular_point_lon.data))
     to_keep_indices = np.argwhere(keep_mask)
-    
+
     filtered_raw_counts = [raw_counts[i, j] for i, j in to_keep_indices]
     output_array = np.full(raw_counts.shape, np.nan, dtype=np.float32)
+
+    #Lat e lon del pont speculare
     specular_point_lats = specular_point_lat[to_keep_indices[:, 0]]
     specular_point_lons = specular_point_lon[to_keep_indices[:, 0]]
-    
+    #Timestamps 
+    filtered_timestamps = np.array([timestamps[i] for i, j in to_keep_indices], dtype=np.float64)
+    time_coverage_start = f.time_coverage_start
+    time_coverage_end = f.time_coverage_end
+
     for idx, (i, j) in enumerate(to_keep_indices):
         output_array[i, j] = filtered_raw_counts[idx]
             
@@ -42,15 +48,17 @@ def preprocess_w_lat_lons(netcdf_file_path):
 
     specular_point_lats = specular_point_lat.ravel()[keep_indices]
     specular_point_lons = specular_point_lon.ravel()[keep_indices]
-    
+    filtered_timestamps = np.array([timestamps[i] for i, j in to_keep_indices])
+
     surface_types = f.variables["sp_surface_type"][:]
     surface_types = np.nan_to_num(surface_types, nan=0)
     surface_types_unravelled = surface_types.ravel()
     label_data = [1 if surface_type in np.arange(1, 8) else 0 for surface_type in surface_types_unravelled]
     label_data = [label_data[lab] for lab in range(len(label_data)) if lab in keep_indices]
 
-    assert np.array(fit_data).shape[0] == len(label_data) == np.array(specular_point_lats).shape[0] == np.array(specular_point_lons).shape[0], \
+
+    assert np.array(fit_data).shape[0] == len(label_data) == np.array(specular_point_lats).shape[0] == np.array(specular_point_lons).shape[0] == filtered_timestamps.shape[0], \
         f"Shape mismatch: fit_data {np.array(fit_data).shape[0]}, label_data {len(label_data)}, lats {np.array(specular_point_lats).shape[0]}, lons {np.array(specular_point_lons).shape[0]}"
     
 
-    return fit_data, label_data, specular_point_lats, specular_point_lons
+    return fit_data, label_data, specular_point_lats, specular_point_lons, filtered_timestamps, time_coverage_start, time_coverage_end
